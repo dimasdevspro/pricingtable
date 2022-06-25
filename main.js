036027c92cae4cd36730825c32f01eaa1815dbd5
dimasdevspro <dimasdevspro@gmail.com>
2022-06-25 14:09:14 -0300

    first comit

---
+++ main.js
@@ -0,0 +1,17 @@
+function freeTableSelected(){
+    const selectedFree = document.querySelector('.free-title')
+    selectedFree.classList.toggle('table-selected');
+    
+}
+
+function basicTableSelected(){
+    const selectedBasic = document.querySelector('.basic-title')
+    selectedBasic.classList.toggle('table-selected');
+    
+}
+
+function profissionalTableSelected(){
+    const selectedProfissional = document.querySelector('.profissional-title')
+    selectedProfissional.classList.toggle('table-selected');
+    
+}
